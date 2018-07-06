3bb57f3d437dd8cf4826a5e15520fdd974faac31
furong <furong.yang@lip6.fr>
2018-07-06 12:28:15 +0000

    modified address assignment schema for AP

--- TSD/IntfOpsDef.py
+++ TSD/IntfOpsDef.py
@@ -3,6 +3,8 @@
 
 import commands as cmds
 import sys
+import socket
+import struct
 import re
 import os
 import logging
@@ -12,160 +14,198 @@ sys.path.append(os.path.abspath(os.path.join("..")))
 
 class IntfOps(object):
 
-	def scan(self, *args, **kwargs):
-		pass
+    def scan(self, *args, **kwargs):
+        pass
 
-	def open(self, *args, **kwargs):
-		pass
-	
-	def close(self, *args, **kwargs):
-		pass
+    def open(self, *args, **kwargs):
+        pass
+    
+    def close(self, *args, **kwargs):
+        pass
 
 class WiFiIntfOps(IntfOps):
 
-	def __init__(self):
-		self.intf_type = INTFTYPE.WIFI
-		self.intf_type_str = "WiFi"
+    def __init__(self):
+        self.intf_type = INTFTYPE.WIFI
+        self.intf_type_str = "WiFi"
 
-	def scan(self, *args, **kwargs):
-		"""
-		Description: select one spare WiFi interface, and 
-		Return: (intf_name, nrg5_ap_info_list, radio_resource_usages) or None
-		"""
-		sh_ret, intf_list = cmds.getstatusoutput("ifconfig -a | grep wlan | grep -v UP | awk -F: '{print $1}'")
-		assert sh_ret==0
-		intf_list = intf_list.split("\n")
-		if len(intf_list) == 0:
-			return None
-		
-		intf = intf_list[0]
-		sh_ret, bss_list = cmds.getstatusoutput('ifconfig {} up && iw dev {} scan | egrep -i "(^BSS)|(DS Parameter set: channel)|(signal:)|(SSID:)" && ifconfig {} down'.format(intf, intf, intf))
-		assert sh_ret==0
+    def scan(self, *args, **kwargs):
+        """
+        Description: select one spare WiFi interface, and 
+        Return: (intf_name, nrg5_ap_info_list, radio_resource_usages) or None
+        """
+        sh_ret, intf_list = cmds.getstatusoutput("ifconfig -a | grep wlan | grep -v UP | awk -F: '{print $1}'")
+        assert sh_ret==0
+        intf_list = intf_list.split("\n")
+        if len(intf_list) == 0:
+            return None
+        
+        intf = intf_list[0]
+        sh_ret, bss_list = cmds.getstatusoutput('ifconfig {} up && iw dev {} scan | egrep -i "(^BSS)|(DS Parameter set: channel)|(signal:)|(SSID:)" && ifconfig {} down'.format(intf, intf, intf))
+        assert sh_ret==0
 
-		bss_list_str = bss_list.split("\n")
-		bss_list = []
+        bss_list_str = bss_list.split("\n")
+        bss_list = []
 
-		for bss_index in range(0, len(bss_list_str), 4):
-			bss_mac = re.match(r"BSS\s*([a-zA-Z0-9:]+).*", bss_list_str[bss_index]).group(1)
-			bss_ss = re.match(r"\s*signal:\s*([-0-9\.]*\s*dBm)\s*", bss_list_str[bss_index+1]).group(1)
-			bss_ssid = re.match(r"\s*SSID:\s*(\S*)\s*", bss_list_str[bss_index+2]).group(1)
-			bss_chl = re.match(r"\D*(\d*)\s*", bss_list_str[bss_index+3]).group(1)
-			bss_chl = int(bss_chl)
-			bss_list.append({"mac":bss_mac, "signal":bss_ss, "ssid": bss_ssid, "channel":bss_chl})
+        for bss_index in range(0, len(bss_list_str), 4):
+            bss_mac = re.match(r"BSS\s*([a-zA-Z0-9:]+).*", bss_list_str[bss_index]).group(1)
+            bss_ss = re.match(r"\s*signal:\s*([-0-9\.]*\s*dBm)\s*", bss_list_str[bss_index+1]).group(1)
+            bss_ssid = re.match(r"\s*SSID:\s*(\S*)\s*", bss_list_str[bss_index+2]).group(1)
+            bss_chl = re.match(r"\D*(\d*)\s*", bss_list_str[bss_index+3]).group(1)
+            bss_chl = int(bss_chl)
+            bss_list.append({"mac":bss_mac, "signal":bss_ss, "ssid": bss_ssid, "channel":bss_chl})
 
-		channel_usage = {}
-		nrg5_bss_list = filter(lambda e:"nrg-5" in e["ssid"], bss_list)
-		for bss in bss_list:
-			channel = bss["channel"]
-			if channel in channel_usage:
-				channel_usage[channel] += 1
-			else:
-				channel_usage[channel] = 0
+        channel_usage = {}
+        nrg5_bss_list = filter(lambda e:"nrg-5" in e["ssid"], bss_list)
+        for bss in bss_list:
+            channel = bss["channel"]
+            if channel in channel_usage:
+                channel_usage[channel] += 1
+            else:
+                channel_usage[channel] = 0
 
-		return intf, nrg5_bss_list, channel_usage
+        return intf, nrg5_bss_list, channel_usage
 
-	def open(self, *args, **kwargs):
-		"""
-		Description: open an wifi hotspot on a given interface, ssid and channel.
-		Parameters: kwargs should include "interface" "ssid" "channel" at least
-		Return: nothing
-		"""
-		if "interface" not in kwargs:
-			raise Exception, "Open wifi hotspot without Interface"
-		if "ssid" not in kwargs:
-			raise Exception, "Open wifi hotspot without SSID"
-		if "channel" not in kwargs:
-			raise Exception, "Open wifi hotspot withou Channel"
-		interface = kwargs["interface"]
-		ssid = kwargs["ssid"]
-		channel = kwargs["channel"]
-		password = "upmc75005"
-		if "password" in kwargs:
-			password = kwargs["password"]
-		#use the gateway as the default dns
-		sh_ret, dns_server = cmds.getstatusoutput("ip route | grep default | gawk --re-interval '{match($0,/([0-9]{1,3}\.){3}[0-9]{1,3}/,a) ;print a[0]}'") 
-		assert sh_ret==0
-		if "dns" in kwargs:
-			dns_server = kwargs["dns"]
-		
-		#interface, connected to upstream node/gateway
-		sh_ret, gw_intf = cmds.getstatusoutput("ip route | grep default | awk -F\"dev \" '{print $2}' | awk '{print $1}'")
-		assert sh_ret==0
+    def __generate_dhcp_range(self, ip, netmask_len):
+        """
+        return: (low ip, high ip)
+        """
+        netmask = (0xffffffff << (32 - netmask_len)) & 0xffffffff
+        low_ip = (ip & netmask) + 1
+        high_ip = ip - 1
+        low_ip_str = socket.inet_ntoa(struct.pack("!I", low_ip))
+        high_ip_str = socket.inet_ntoa(struct.pack("!I", high_ip))
+        return low_ip_str, high_ip_str
 
-		#stop hotspot related services
-		sh_ret = os.system("service hostapd stop && service dnsmasq stop")
-		assert sh_ret==0
+    def __generate_addr_info(self, ip, netmask_len):
+        """
+        return: (ip_str, netmask_str, subnet_str, subnet_len)
+        """
+        netmask = (0xffffffff << (32 - netmask_len)) & 0xffffffff
+        subnet = ip & netmask
 
-		#rewrite the interface confiuration file, /etc/network/interfaces.d/<intfname>
-		with open("/etc/network/interfaces.d/{}".format(interface), "w") as f:
-			f.write("iface {} inet static\n".format(interface))
-			f.write("    address 10.0.0.254\n")
-			f.write("    netmask 255.255.255.0\n")
+        ip_str = socket.inet_ntoa(struct.pack("!I", ip))
+        netmask_str = socket.inet_ntoa(struct.pack("!I", netmask))
+        subnet_str = socket.inet_ntoa(struct.pack("!I", subnet))
 
-		#bring up interface
-		sh_ret = os.system("ifup {}".format(interface))
-		assert sh_ret==0
+        return (ip_str, netmask_str, subnet_str, netmask_len)
 
-		#rewrite the hostapd configuration file, /etc/hostapd/hostapd.conf
-		with open("/etc/hostapd/hostapd.conf", "w") as f:
-			f.write("""interface={}
-					ssid={}
-					hw_mode=g
-					channel={}
-					ieee80211n=1
-					ieee80211d=1
-					country_code=FR
-					wmm_enabled=1
-					auth_algs=1
-					wpa=2
-					wpa_passphrase={}
-					wpa_key_mgmt=WPA-PSK
-					rsn_pairwise=CCMP""".format(interface, ssid, channel, password).replace(" ","").replace("\t", ""))
 
-		#rewrite the dnsmasq configuration file, /etc/dnsmasq.conf
-		with open("/etc/dnsmasq.conf", "w") as f:
-			f.write("""interface={}
-					listen-address=10.0.0.254
-					bind-interfaces
-					server={}
-					domain-needed
-					bogus-priv
-					dhcp-range=10.0.0.1,10.0.0.253,255.255.255.0,24h""".format(interface, dns_server).replace(" ", "").replace("\t", ""))
+    def open(self, *args, **kwargs):
+        """
+        Description: open an wifi hotspot on a given interface, ssid and channel.
+        Parameters: kwargs should include "interface" "ssid" "channel" "ip" "netmask_len" at least
+        Return: nothing
+        """
+        if "interface" not in kwargs:
+            raise Exception, "Open wifi hotspot without Interface"
+        if "ssid" not in kwargs:
+            raise Exception, "Open wifi hotspot without SSID"
+        if "channel" not in kwargs:
+            raise Exception, "Open wifi hotspot without Channel"
+        if "ip" not in kwargs or "netmask_len" not in kwargs:
+            raise Exception, "Open wifi hotspot without assigning IP and netmask_len"
+        interface = kwargs["interface"]
+        ssid = kwargs["ssid"]
+        channel = kwargs["channel"]
+        password = "upmc75005"
+        ip = kwargs["ip"]
+        netmask_len = kwargs["netmask_len"]
+ 
+        if "password" in kwargs:
+            password = kwargs["password"]
+        #use the gateway as the default dns
+        sh_ret, dns_server = cmds.getstatusoutput("ip route | grep default | gawk --re-interval '{match($0,/([0-9]{1,3}\.){3}[0-9]{1,3}/,a) ;print a[0]}'") 
+        assert sh_ret==0
+        if "dns" in kwargs:
+            dns_server = kwargs["dns"]
+        
+        #interface, connected to upstream node/gateway
+        sh_ret, gw_intf = cmds.getstatusoutput("ip route | grep default | awk -F\"dev \" '{print $2}' | awk '{print $1}'")
+        assert sh_ret==0
 
-		#enable ip forwarding 
-		sh_ret = os.system("sysctl -w net.ipv4.conf.all.forwarding=1")
-		assert sh_ret==0
+        #stop hotspot related services
+        sh_ret = os.system("service hostapd stop && service dnsmasq stop")
+        assert sh_ret==0
 
-		#add MASQUERADE NAT rules to iptable
-		sh_ret = os.system("iptables -A INPUT -i {} -j ACCEPT".format(interface))
-		assert sh_ret==0
-		sh_ret = os.system("iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o {} -j MASQUERADE".format(gw_intf))
+        #clear iptables
+        sh_ret = os.system("iptables -F && iptables -t nat -F")
+        assert sh_ret==0
 
-		#restart hostapd & dnsmasq
-		sh_ret = os.system("service hostapd start && service dnsmasq start")
-		assert sh_ret==0
+        #get addr info
+        ip_str, netmask_str, subnet_str, subnet_len = self.__generate_addr_info(ip, netmask_len)
 
-	def close(self, *args, **kwargs):
-		"""
-		Description: close hotspot on specific interface
-		Parameters: kwargs should contain "interface" at least
-		Return: nothing
-		"""
-		if "interface" not in kwargs:
-			raise Exception, "Close wifi hotspot without Interface"
-		interface = kwargs["interface"]
+        #rewrite the interface confiuration file, /etc/network/interfaces.d/<intfname>
+        with open("/etc/network/interfaces.d/{}".format(interface), "w") as f:
+            f.write("iface {} inet static\n".format(interface))
+            f.write("    address {}\n".format(ip_str))
+            f.write("    netmask {}\n".format(netmask_str))
 
-		#shutdown interface
-		assert os.system("ifdown {}".format(interface))==0
+        #bring up interface
+        sh_ret = os.system("ifup {}".format(interface))
+        assert sh_ret==0
 
-		#stop service
-		assert os.system("service hostapd stop && service dnsmasq stop")==0
+        #rewrite the hostapd configuration file, /etc/hostapd/hostapd.conf
+        with open("/etc/hostapd/hostapd.conf", "w") as f:
+            f.write("""interface={}
+                    ssid={}
+                    hw_mode=g
+                    channel={}
+                    ieee80211n=1
+                    ieee80211d=1
+                    country_code=FR
+                    wmm_enabled=1
+                    auth_algs=1
+                    wpa=2
+                    wpa_passphrase={}
+                    wpa_key_mgmt=WPA-PSK
+                    rsn_pairwise=CCMP""".format(interface, ssid, channel, password).replace(" ","").replace("\t", ""))
 
-		#delete configuration file, hostapd, dnsmasq, interfaces.d/intf
-		assert os.system("mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.lastTSD")==0
-		assert os.system("mv /etc/dnsmasq.conf /etc/dnsmasq.conf.lastTSD")==0
-		assert os.system("rm -f /etc/network/interfaces.d/{}".format(interface))==0
-	
-		#delete NAT
-		assert os.system("iptables -F && iptables -t nat -F")==0
+        low_ip_str, high_ip_str = self.__generate_dhcp_range(ip, netmask_len)
 
+        #rewrite the dnsmasq configuration file, /etc/dnsmasq.conf
+        with open("/etc/dnsmasq.conf", "w") as f:
+            f.write("""interface={}
+                    listen-address={}
+                    bind-interfaces
+                    server={}
+                    domain-needed
+                    bogus-priv
+                    dhcp-range={},{},{},24h""".format(interface, ip_str, dns_server, low_ip_str, high_ip_str, netmask_str).replace(" ", "").replace("\t", ""))
+
+        #enable ip forwarding 
+        sh_ret = os.system("sysctl -w net.ipv4.conf.all.forwarding=1")
+        assert sh_ret==0
+
+        #add MASQUERADE NAT rules to iptable
+        sh_ret = os.system("iptables -A INPUT -i {} -j ACCEPT".format(interface))
+        assert sh_ret==0
+        sh_ret = os.system("iptables -t nat -A POSTROUTING -s {}/{} -o {} -j MASQUERADE".format(subnet_str, subnet_len, gw_intf))
+
+        #restart hostapd & dnsmasq
+        sh_ret = os.system("service hostapd start && service dnsmasq start")
+        assert sh_ret==0
+
+    def close(self, *args, **kwargs):
+        """
+        Description: close hotspot on specific interface
+        Parameters: kwargs should contain "interface" at least
+        Return: nothing
+        """
+        if "interface" not in kwargs:
+            raise Exception, "Close wifi hotspot without Interface"
+        interface = kwargs["interface"]
+
+        #shutdown interface
+        assert os.system("ifdown {}".format(interface))==0
+
+        #stop service
+        assert os.system("service hostapd stop && service dnsmasq stop")==0
+
+        #delete configuration file, hostapd, dnsmasq, interfaces.d/intf
+        assert os.system("mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.lastTSD")==0
+        assert os.system("mv /etc/dnsmasq.conf /etc/dnsmasq.conf.lastTSD")==0
+        assert os.system("rm -f /etc/network/interfaces.d/{}".format(interface))==0
+    
+        #delete NAT
+        assert os.system("iptables -F && iptables -t nat -F")==0
--- TSD/TSD.py
+++ TSD/TSD.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-
-
 import sys, os
 sys.path.append(os.path.abspath(os.path.join("..")))
 
@@ -14,154 +12,213 @@ import struct
 from common.Def import INTFTYPE, MSG_HDR_LEN, MSGTYPE
 from IntfOpsDef import WiFiIntfOps, IntfOps
 
-
 class TSD(object):
 
-	def __init__(self, *args, **kwargs):
-		"""
-		Description: constructor of TSD
-		Parameters: kwargs may contain "intfops" which is a list storing supported interface ops object, "5Gintf" means name of 5G interface
-		Return: nothing
-		"""
-		self.active_intfs = []
-		self.intf_mods = {}
-		self.__load_device_info(**kwargs)
-		if "intfops" in kwargs:
-			for ops in kwargs["intfops"]:
-				self.register_intf_module(ops)
-		if "5Gintf" in kwargs:
-			self.intf_5g = kwargs["5Gintf"]
-		else:
-			self.intf_5g = "eth0"
-		self.is_5g_gw = self.__is_5G_gateway()
-		
-		self.__load_vTSD_info(**kwargs)
+    def __init__(self, *args, **kwargs):
+        """
+        Description: constructor of TSD
+        Parameters: kwargs may contain "intfops" which is a list storing supported interface ops object, "5Gintf" means name of 5G interface
+        Return: nothing
+        """
+        self.active_intfs = []
+        self.intf_mods = {}
+        self.__load_device_info(**kwargs)
+        if "intfops" in kwargs:
+            for ops in kwargs["intfops"]:
+                self.register_intf_module(ops)
+        
+        self.is_5g_gw = self.__is_5G_gateway(**kwargs)
+        self.__prepare_ap_address_pool(self.is_5g_gw)
+        self.__load_vTSD_info(**kwargs)
 
-	def __is_5G_gateway(self):
-		"""
-		Description: check if me is a 5G gateway
-		Parameters: nothing
-		Return: True or False	
-		"""
-		# FIXME: Probably, we should change the method to judge if a device is a 5G gateway	
-		# if the default route through self.intf_5g, we say that the device is a 5G gateway
-		sh_ret, gw_intf = cmds.getstatusoutput("ip route | grep default | awk -F\"dev \" '{print $2}' | awk '{print $1}'")
-		assert sh_ret==0
+    def __prepare_ap_address_pool(self, is_5g_gw):
+        """
+        Description: generate address pool
+        """ 
+        #FIXME: maybe we will use flexible netmask_len in the future
+        self.subnet_len = 24
+        if is_5g_gw:
+            #10.0.0.0/24
+            #10.0.1.0/24
+            self.base_ap_subnet = 0xa000000
+            self.current_ap_subnet = 0xa000100
+            return
 
-		return gw_intf == self.intf_5g
+        #get gw ip to avoid use same subnet with gw interface
+        sh_ret, gw_ip= cmds.getstatusoutput("ip route | grep default | gawk --re-interval '{match($0,/([0-9]{1,3}\.){3}[0-9]{1,3}/,a) ;print a[0]}'")
+        assert sh_ret == 0
 
-	def __load_vTSD_info(self, **kwargs):
-		
-		# FIXME: currently, we should fixed url for vTSD, and we should setup manual dns-mapping in /etc/hosts
-		self.vTSD_url = "nrg5.vtsd.fr" 
-		self.vTSD_ip = socket.gethostbyname(self.vTSD_url)
-		self.vTSD_port = 6666
+        netmask_len = self.subnet_len
+        gw_ip_uint = struct.unpack("!I", socket.inet_aton(gw_ip))
+        netmask = (0xffffffff << (32-netmask_len)) & 0xffffffff
+        
+        temp = (gw_ip_uint >> (32-netmask_len)) + 1
+        if temp == (0xffffffff>>(32-netmask_len)):
+            temp += 2
 
-	def __load_device_info(self, **kwargs):
-		"""
-		Description: load device information from a json file
-		Parameters: the file is located at /etc/nrg-5/device.json unless "devinfo" is specified in kwargs
-		Return: nothing
-		"""
-		devinfo_path = "/etc/nrg-5/device.json"
-		if "devinfo" in kwargs:
-			devinfo_path = kwargs["devinfo"]
-		
-		with open(devinfo_path, "r") as f:
-			devinfo = json.load(f)
-			self.device_id = devinfo[u"device_id"]
-			self.credential = devinfo[u"credential"]
+        subnet = (temp << (32-netmask_len)) & 0xffffffff
 
-	def register_intf_module(self, module_ops):
-		assert isinstance(module_ops, IntfOps)
-		assert hasattr(module_ops, "intf_type")
-		intf_type = module_ops.intf_type
-		assert intf_type in INTFTYPE
-		if intf_type not in self.intf_mods:
-			self.intf_mods[intf_type] = module_ops
-			logging.info("TSD register the {} module".format(module_ops.intf_type_str))
-		else:
-			logging.warning("TSD try to register the {} module that was registered".format(module_ops.intf_type_str))
+        self.base_ap_subnet = gw_ip_uint & netmask
+        self.current_ap_subnet = subnet
+    
+    def __assign_ap_address(self):
+        """
+        Return: (ip, netmask_len)
+        """
+        netmask_len = self.subnet_len
+        subnet = self.current_ap_subnet
+        
+        #update self.current_ap_subnet
+        self.current_ap_subnet = (self.current_ap_subnet >> (32-netmask_len)) + 1
+        if self.current_ap_subnet == (0xffffffff>>(32-netmask_len)):
+            self.current_ap_subnet += 2
+        self.current_ap_subnet = (self.current_ap_subnet << (32-netmask_len)) & 0xffffffff
 
-	def __connect_to_vTSD(self):
-		"""
-		Description: connect to vTSD and return the socket
-		"""
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.connect((self.vTSD_ip, self.vTSD_port))
-		return sock
+        if self.current_ap_subnet == self.base_ap_subnet:
+            raise Exception, "TSD: AP address space is used up"
 
-	def __generate_device_register_msg(self):
-		msg = {"device_id": self.device_id, "credential": self.credential, "intfs":[]}
-		for t, v in self.intf_mods.iteritems():
-			intf_name, nrg5_ap_list, radio_usage = v.scan()
-			msg["intfs"].append({"type":v.intf_type.value, "typename":v.intf_type_str, "name":intf_name, "radio_usage":radio_usage})
-		msg = json.dumps(msg)
-		msg = struct.pack("!BH{}s".format(len(msg)), MSGTYPE.DEV_REG.value, len(msg), msg)
-		return msg
-
-	def __receive_msg_hdr(self, sock):
-		msg_hdr = sock.recv(MSG_HDR_LEN)
-		tp, length = struct.unpack("!BH", msg_hdr)
-		return MSGTYPE(tp), length
-
-	def __process_dev_reg_reply(self, sock, tp, length):
-		assert tp == MSGTYPE.DEV_REG_REPLY
-		reply = json.loads(sock.recv(length))
-		assert reply[u"device_id"]==self.device_id
-		if reply[u"reg_result"]==True:
-			#open hotspot
-			for intf in reply[u"intfs"]:
-				intf_type = INTFTYPE(intf[u"type"])
-				intf_name = intf[u"name"].encode("utf-8")
-				intf_channel = intf[u"channel"]
-				intf_ssid = intf[u"ssid"].encode("utf-8")
-				self.intf_mods[intf_type].open(interface=intf_name, ssid=intf_ssid, channel=intf_channel)
-				self.active_intfs.append({"type":intf_type, "name":intf_name})
-			return True
-		
-		return False
-
-	def start_service(self):
-		#connect to vTSD
-		sock = self.__connect_to_vTSD()
-		
-		msg = self.__generate_device_register_msg()
-		sock.send(msg)
-	
-		tp, length = self.__receive_msg_hdr(sock)
-		ret = self.__process_dev_reg_reply(sock, tp, length)
-			
-		sock.close()
-
-		return ret
-
-	def stop_service(self):
-		
-		for intf in self.active_intfs:
-			self.intf_mods[intf["type"]].close(interface=intf["name"])
+        ip = subnet | ((0xffffffff >> netmask_len) - 1)
+        return ip, self.subnet_len
 
 
-	def baba(self):
-		pass
-		#register me to vTSD
-	
-		#start forwarding service for downstream device
+    def __is_5G_gateway(self, **kwargs):
+        """
+        Description: check if me is a 5G gateway
+        Parameters: nothing
+        Return: True or False    
+        """
+        # FIXME: Probably, we should change the method to judge if a device is a 5G gateway    
+        # if the default route through self.intf_5g, we say that the device is a 5G gateway
 
-		#exposure my service
-	
+        if "5Gintf" in kwargs:
+            self.intf_5g = kwargs["5Gintf"]
+        else:
+            self.intf_5g = "eth0"
+       
+        sh_ret, gw_intf = cmds.getstatusoutput("ip route | grep default | awk -F\"dev \" '{print $2}' | awk '{print $1}'")
+        assert sh_ret==0
+
+        return gw_intf == self.intf_5g
+
+    def __load_vTSD_info(self, **kwargs):
+        
+        # FIXME: currently, we should fixed url for vTSD, and we should setup manual dns-mapping in /etc/hosts
+        self.vTSD_url = "nrg5.vtsd.fr" 
+        self.vTSD_port = 6666
+        if "vTSD_url" in kwargs:
+            self.vTSD_url = kwargs["vTSD_url"]
+        if "vTSD_port" in kwargs:
+            self.vTSD_port = kwargs["vTSD_port"]
+
+        self.vTSD_ip = socket.gethostbyname(self.vTSD_url)
+
+    def __load_device_info(self, **kwargs):
+        """
+        Description: load device information from a json file
+        Parameters: the file is located at /etc/nrg-5/device.json unless "devinfo" is specified in kwargs
+        Return: nothing
+        """
+        devinfo_path = "/etc/nrg-5/device.json"
+        if "devinfo" in kwargs:
+            devinfo_path = kwargs["devinfo"]
+        
+        with open(devinfo_path, "r") as f:
+            devinfo = json.load(f)
+            self.device_id = devinfo[u"device_id"]
+            self.credential = devinfo[u"credential"]
+
+    def register_intf_module(self, module_ops):
+        assert isinstance(module_ops, IntfOps)
+        assert hasattr(module_ops, "intf_type")
+        intf_type = module_ops.intf_type
+        assert intf_type in INTFTYPE
+        if intf_type not in self.intf_mods:
+            self.intf_mods[intf_type] = module_ops
+            logging.info("TSD register the {} module".format(module_ops.intf_type_str))
+        else:
+            logging.warning("TSD try to register the {} module that was registered".format(module_ops.intf_type_str))
+
+    def __connect_to_vTSD(self):
+        """
+        Description: connect to vTSD and return the socket
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.vTSD_ip, self.vTSD_port))
+        return sock
+
+    def __generate_device_register_msg(self):
+        msg = {"device_id": self.device_id, "credential": self.credential, "intfs":[]}
+        for t, v in self.intf_mods.iteritems():
+            intf_name, nrg5_ap_list, radio_usage = v.scan()
+            msg["intfs"].append({"type":v.intf_type.value, "typename":v.intf_type_str, "name":intf_name, "radio_usage":radio_usage})
+        msg = json.dumps(msg)
+        msg = struct.pack("!BH{}s".format(len(msg)), MSGTYPE.DEV_REG.value, len(msg), msg)
+        return msg
+
+    def __receive_msg_hdr(self, sock):
+        msg_hdr = sock.recv(MSG_HDR_LEN)
+        tp, length = struct.unpack("!BH", msg_hdr)
+        return MSGTYPE(tp), length
+
+    def __process_dev_reg_reply(self, sock, tp, length):
+        assert tp == MSGTYPE.DEV_REG_REPLY
+        reply = json.loads(sock.recv(length))
+        assert reply[u"device_id"]==self.device_id
+        if reply[u"reg_result"]==True:
+            #open hotspot
+            for intf in reply[u"intfs"]:
+                intf_type = INTFTYPE(intf[u"type"])
+                intf_name = intf[u"name"].encode("utf-8")
+                intf_channel = intf[u"channel"]
+                intf_ssid = intf[u"ssid"].encode("utf-8")
+                intf_ip, intf_netmask_len = self.__assign_ap_address()
+                self.intf_mods[intf_type].open(interface=intf_name, ssid=intf_ssid, channel=intf_channel, ip=intf_ip, netmask_len=intf_netmask_len)
+                self.active_intfs.append({"type":intf_type, "name":intf_name})
+            return True
+        
+        return False
+
+    def start_service(self):
+        #connect to vTSD
+        sock = self.__connect_to_vTSD()
+        
+        #send registration request to vTSD
+        msg = self.__generate_device_register_msg()
+        sock.send(msg)
+    
+        #recv registration reply from vTSD
+        tp, length = self.__receive_msg_hdr(sock)
+        
+        ret = self.__process_dev_reg_reply(sock, tp, length)
+         
+        #close the connection to vTSD   
+        sock.close()
+        return ret
+
+    def stop_service(self):
+        
+        for intf in self.active_intfs:
+            self.intf_mods[intf["type"]].close(interface=intf["name"])
+
+    def baba(self):
+        pass
+        #register me to vTSD
+    
+        #start forwarding service for downstream device
+
+        #exposure my service
 
 #testing code
 if __name__ == "__main__":
-	#wifi_ops = WiFiIntfOps()
-	#print wifi_ops.scan()
-	#wifi_ops.open(interface="wlan0", ssid="nrg-5-000000", channel="11")
-	#wifi_ops.close(interface="wlan0")
+    #wifi_ops = WiFiIntfOps()
+    #print wifi_ops.scan()
+    #wifi_ops.open(interface="wlan0", ssid="nrg-5-000000", channel="11")
+    #wifi_ops.close(interface="wlan0")
 
-	tsd = TSD(intfops=[WiFiIntfOps()])
-	while tsd.start_service() == False:
-		#retry after 3s
-		time.sleep(3)
+    tsd = TSD(intfops=[WiFiIntfOps()])
+    while tsd.start_service() == False:
+        #retry after 3s
+        time.sleep(3)
 
-	print raw_input("Enter any key to stop service")
-	tsd.stop_service()
+    print raw_input("Enter any key to stop service")
+    tsd.stop_service()
