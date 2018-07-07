0a64717989270ccd2aa7c290e13c0c6fe8579294
lucadm94 <luca.demori.it@gmail.com>
2018-07-07 23:15:43 +0200

    added Authorizer siimulation, message type handler

--- vTSD/AP_manager.py
+++ vTSD/AP_manager.py
@@ -6,8 +6,7 @@ import json, struct
 import logging
 
 sys.path.append(os.path.abspath(os.path.join("..")))
-
-from common.Def import MSG_HDR_LEN, MSGTYPE, INTFTYPE
+from common.Def import MSG_HDR_LEN, MSGTYPE, INTFTYPE, ERRTYPE
 
 from optparse import OptionParser
 
@@ -21,9 +20,9 @@ class Listener(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
-        logging.info("Listener is started on port " + str(port))
         ### set log destination
         logging.basicConfig(filename="AP_manager.log", level=logging.DEBUG)
+        logging.info("Listener is started on port " + str(port))
 
     def listen(self):
         self.sock.listen(5)
@@ -41,41 +40,61 @@ class Listener(object):
         msg = json.dumps(msg)
         msg = struct.pack("!BH{}s".format(len(msg)), MSGTYPE.DEV_REG_REPLY.value, len(msg), msg)
         return msg
+
+    def __genrate_authentication_failed_message(self):
+        msg = {"err_type": ERRTYPE.AUTH_FAILED.value, "err_msg": "Authentication Failed"}
+        msg = json.dumps(msg)
+        msg = struct.pack("!BH{}s".format(len(msg)), MSGTYPE.REG_FAILED.value, len(msg), msg)
+        return msg
+
+    def __generate_unsupported_msgtype_err(self, in_msg):
+        logging.warning("Received UNSUPPORTED message: "+in_msg)
+        msg = {"supported_msgs": []}
+        msg["supported_msgs"] = ({"msg_type": MSGTYPE.DEV_REG.value, "msg_typename": "Device Registration"}, {"msg_type": MSGTYPE.SERVICE_EXPOSURE.value, "msg_typename": "Service Exposure"})
+        msg = json.dumps(msg)
+        msg = struct.pack("!BH{}s".format(len(msg)), MSGTYPE.UNSUPPORTED_MSGTYPE_ERROR.value, len(msg), msg)
+        return msg
                  
     def __receive_msg_hdr(self, sock):
         msg_hdr = sock.recv(MSG_HDR_LEN)
-        if msg_hdr:
+        if msg_hdr and len(msg_hdr)>2:
+            logging.debug("Unpacking header "+msg_hdr)
             tp, length = struct.unpack("!BH", msg_hdr) # lenght is the length of the json data
-            return (MSGTYPE(tp), length)
-        return None
+            return (MSGTYPE(tp), length, msg_hdr)
+        return (None, None, msg_hdr)
                      
     def __process_dev_reg(self, sock, tp, length):
         data = sock.recv(length)
+        logging.info("Received data: "+data+"\n")
+
         reg = json.loads(data)
-        if __device_is_authenticated(reg[u"device_id"], reg[u"credentials"]):
+        if self.__device_is_authenticated(reg[u"device_id"], reg[u"credential"]):
             msg = self.__generate_device_reg_reply(reg)
         else:
-            msg = #TODO = add reg_fail_msg
+            msg = self.__genrate_authentication_failed_message()
         return msg
 
     def __listen_to_client(self, client, address):
         size = 1024
         while True:
             try:
-                msg_tp, datalenght = self.__receive_msg_hdr(client)  # data[0] = msg type; data[1] = msg length
+                msg_tp, datalenght, hdr_str = self.__receive_msg_hdr(client)  # data[0] = msg type; data[1] = msg length
+                logging.info("Received message type [{}]:{}".format(msg_tp.value if msg_tp else "UNRECOGNIZED", msg_tp))
                 # FIXME: extend message discriminator
                 if msg_tp == MSGTYPE.DEV_REG:
                     response = self.__process_dev_reg(client, msg_tp, datalenght)
                 else:
-                    response = "failed "# not handled
+                    in_msg = hdr_str+client.recv(size)
+                    response =self.__generate_unsupported_msgtype_err(in_msg)
 
 
                 client.send(response)
                 logging.debug("Replying to " + str(address) + " with " + str(response))
 
             # raise Exception, ('Client Disconnected')
-            except msg:
-                print "AP_manager: error "
+            except:
+                print "AP_manager: error ",sys.exc_info()
+                logging.error( "AP_manager: error " + str(sys.exc_info()))
                 client.close()
                 return False
 
