cfa6803fd6c152c67fc5aecf1b9c01c1ed4cb48b
lucadm94 <luca.demori.it@gmail.com>
2018-07-07 23:14:50 +0200

    added:   - message types:     - REG_FALED     - UNSUPPORTED_MSGTYPE_ERROR
      - ERRORS:
        - AUTH_FAILED

--- common/Def.py
+++ common/Def.py
@@ -1,27 +1,1 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-from enum import Enum
-
-class INTFTYPE(Enum):
-	WIFI = 1
-
-#1B type + 2B len
-MSG_HDR_LEN = 3
-
-class MSGTYPE(Enum):
-	DEV_REG = 0x01
-	DEV_REG_REPLY = 0x02
-	SERVICE_EXPOSURE = 0x03
-	SERVICE_EXPOSURE_REPLY = 0x04
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+#!/usr/bin/env python#-*- coding:utf-8 -*-from enum import Enumclass INTFTYPE(Enum):    WIFI = 1#1B type + 2B lenMSG_HDR_LEN = 3class MSGTYPE(Enum):    DEV_REG = 0x01    DEV_REG_REPLY = 0x02    SERVICE_EXPOSURE = 0x03    SERVICE_EXPOSURE_REPLY = 0x04    REG_FAILED = 0x05    UNSUPPORTED_MSGTYPE_ERROR = 0x10class ERRTYPE(Enum):    AUTH_FAILED = 0x11class bcolors:    HEADER = '\033[95m'    OKBLUE = '\033[94m'    OKGREEN = '\033[92m'    WARNING = '\033[93m'    FAIL = '\033[91m'    ENDC = '\033[0m'    BOLD = '\033[1m'    UNDERLINE = '\033[4m'
